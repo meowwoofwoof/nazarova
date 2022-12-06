6bb0098cf0f7179cd55eb54587867f90df02bc22
мяугав <114457221+meowwoofwoof@users.noreply.github.com>
2022-12-06 09:32:39 +0700

    Create кнопка.py

---
+++ кнопка.py
@@ -0,0 +1,22 @@
+<style>
+.button_1670292822832 {
+    display: inline-block !important;
+    text-decoration: none !important;
+    background-color: #ff2a2a !important;
+    color: #630808 !important;
+    border: 6px solid #5f101e !important;
+    border-radius: 5px !important;
+    font-size: 29px !important;
+    padding: 10px 50px !important; 
+    transition: all 0.6s ease !important;
+}
+.button_1670292822832:hover{
+    text-decoration: none !important; 
+    background-color: #18191a !important;
+    color: #ffffff !important;
+    border-color: #2f383b !important;
+}
+</style>
+<a href="https://github.com/meowwoofwoof/nazarova" class="button_1670292822832" target="_blank">
+  бахай
+</a>
